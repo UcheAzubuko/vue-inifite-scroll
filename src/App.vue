1e6909c8d188cb59f756bd3d31793872175270f6
UcheAzubuko <azubukouche@yahoo.com>
2022-09-02 21:00:02 +0100

    Init

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,69 @@
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
+  <!-- <div ref="test" v-show="show" v-observe-visibility="visibilityChanged">Hello world!</div>
+  <div v-observe-visibility="handleInfinityScroll"></div> -->
 </template>
+
+
+<script lang="ts">
+  type Course = {
+    id: string;
+    courseTitle: string;
+    titleSummary: string;
+    companyName: string;
+    price: string;
+    courseLevel: string;
+    discount: string;
+    duration: string;
+    currency: string;
+    fileUrl: string;
+  };
+  
+  import { defineComponent, ref, onMounted } from "vue";
+  import axios from "axios";
+  
+  export default defineComponent({
+    name: "Home",
+  
+    components: {},
+  
+    setup() {
+      const allCourses = ref<Course[]>([]);
+  
+      const fecthAllCourses = async () => {
+        let courses = await axios.get(
+          `http://localhost:8000/api/v1/marketplace/courses?lastId=0`
+        );
+  
+        allCourses.value = courses.data.body;
+      };
+  
+      const bottomIsVisible = ref(true);
+      const show = ref(true);
+  
+      const visibilityChanged = (isVisible: boolean, entry: unknown) => {
+        bottomIsVisible.value = isVisible;
+        console.log(entry)
+      };
+  
+      const handleInfinityScroll = () => {
+        console.log("fetch more stuff");
+      };
+  
+      onMounted(() => {
+        fecthAllCourses();
+      });
+  
+      return {
+        allCourses,
+        visibilityChanged,
+        handleInfinityScroll,
+        show,
+      };
+    },
+  });
+  </script>
 
 <style>
 #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ul >
+      <li v-for="course in allCourses" :key="course?.id">
+        <p>summmary: {{ course.titleSummary }}</p>
+        <p>store name: {{ course.companyName }}</p>
+        <p>title: {{ course.courseTitle }}</p>
+        <p>level: {{ course.courseLevel }}</p>
+        <p>{{ course.currency }} {{ course.price }}</p>
+        <p>{{ course.discount }}% discount</p>
+        <p>id: {{ course.id }}</p>
+      </li>
+    </ul>
+    <!-- <Observer /> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+type Course = {
+  id: string;
+  courseTitle: string;
+  titleSummary: string;
+  companyName: string;
+  price: string;
+  courseLevel: string;
+  discount: string;
+  duration: string;
+  currency: string;
+  fileUrl: string;
+};
+
+import { defineComponent, ref } from "vue";
+import axios from "axios";
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: "Home",
+
+  setup() {
+    const allCourses = ref<Course[]>([]);
+
+    const fecthAllCourses = async () => {
+      let courses = await axios.get(
+        `http://localhost:8000/api/v1/marketplace/courses?lastId=0`
+      );
+
+      allCourses.value = courses.data.body;
+      console.log(((allCourses.value[allCourses.value.length - 1]).id).toString());
+    };
+
+    const loadMoreCourses = async () => {
+      let lastId = (allCourses.value[allCourses.value.length - 1].id).toString();
+      let courses = await axios.get(
+        `http://localhost:8000/api/v1/marketplace/courses?lastId=${lastId}`
+      );
+    };
+
+
+    return {
+      allCourses,
+    };
   },
 });
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-auto-rows: auto;
+  gap: 25px;
+  width: 100%;
+}
+
+li {
+  padding: 10px 20px;
+  background: #ffffff;
+  box-shadow: 0px 0px 1px rgba(66, 71, 76, 0.32),
+    0px 4px 8px rgba(66, 71, 76, 0.06), 0px 8px 48px #eeeeee;
+  border-radius: 8px;
+}
+</style>
